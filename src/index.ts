6433a189fdfa618a34061fd684fe4c98c3d46d6a
Ramco <ramazane791@gmail.com>
2022-07-27 22:37:57 +0300

    ilk commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+'use strict';
+import express, { Request, Response } from 'express';
+import fs from 'fs';
+import path from 'path';
+import multer from 'multer';
+import "dotenv/config"
+
+const app = express();
+
+app.use("/u", express.static("uploads"));
+
+app.get("/", (req: Request, res: Response) => {
+    res.send({ message: true })
+})
+
+app.listen(5000, () => console.log("ready"))
