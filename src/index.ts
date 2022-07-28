cec276d20f2104257d49a6b92f2a43c24b8d2617
Ramco <ramazane791@gmail.com>
2022-07-28 17:01:46 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ app.post("/upload", upload.single("images"), (req: any, res: Response) => {
     if (req.body.secret === process.env.RAMCHO_SECRET) {
         if (!req.file) return res.json({ error: "File not found", status: 404 })
         var filename = createUniqId()
-        const file = new File({ _id: filename, buffer: req.file.buffer, type: req.file.mimetype.split("/")[1] })
+        const file = new File({ _id: filename, buffer: req.file.buffer, type: req.file.mimetype })
         file.save()
         res.json({
             thumbnail_url: "https://ramcho.xyz/u/" + filename,
@@ -44,17 +44,27 @@ app.post("/upload", upload.single("images"), (req: any, res: Response) => {
     }
 })
 
+app.get("/i/:id", async (req, res) => {
+    try {
+        const file: any = await File.findById(req.params.id)
+        if (!req.params.id) return res.status(403).json({ error: "Not enough id" })
+        res.json({ filename: file._id, type: file.type, url: `https://api.ramcho.xyz/u/${file._id}.${file.type.split("/")[1]}` })
+    } catch (err) {
+        console.log(err)
+        res.status(403).json({ error: "Not enough id", status: 403 })
+    }
+})
 app.get("/u/:id", async (req: any, res: any) => {
     try {
         const fileId = req.params.id.split(".")
         if (fileId.length === 1) return res.status(404).json({ error: "File not found", status: 404 })
         if (!fileId[1].match(/^(jpe?g|png|gif|mp4)$/)) return res.status(404).json({ error: "File not found", status: 404 })
         const file: any = await File.findById(fileId[0])
-        if (file.type !== fileId[1]) return res.status(404).json({ error: "File not found", status: 404 })
+        if (file.type.split("/")[1] !== fileId[1]) return res.status(404).json({ error: "File not found", status: 404 })
         res.end(file.buffer)
     } catch (err) {
         console.log(err)
-        res.json({ error: "File not found", status: 404 })
+        res.status(404).json({ error: "File not found", status: 404 })
     }
 })
 
