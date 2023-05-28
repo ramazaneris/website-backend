9e249df5ad2f3271d983c0925f437a11a8eb6b6a
Ramco <ramazane791@gmail.com>
2023-05-28 10:10:20 +0300

    added content type

--- src/index.ts
+++ src/index.ts
@@ -1,80 +1,99 @@
-'use strict';
-import express, { Request, Response } from 'express';
-import fs, { renameSync } from 'fs';
-import path from 'path';
-import multer from 'multer';
-import "dotenv/config"
-import mongoose from 'mongoose'
-import File from '../schema/fileSchema'
-import cors from 'cors'
+"use strict";
+import express, { Request, Response } from "express";
+import fs, { renameSync } from "fs";
+import path from "path";
+import multer from "multer";
+import "dotenv/config";
+import mongoose from "mongoose";
+import File from "../schema/fileSchema";
+import cors from "cors";
 
-mongoose.connect(`${process.env.MONGODB_URI}`)
+mongoose.connect(`${process.env.MONGODB_URI}`);
 
 const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: [`${process.env.CORS_ORIGIN}`, `${process.env.CORS_ORIGIN_SECOND}`],
     credentials: true,
     methods: ["GET,POST"],
-}))
-app.use(express.json())
+  })
+);
+app.use(express.json());
 
 const createUniqId = () => {
-    return (
-        new Date().getTime().toString(36).substring(36) +
-        Math.random().toString(36).slice(3)
-    );
+  return (
+    new Date().getTime().toString(36).substring(36) +
+    Math.random().toString(36).slice(3)
+  );
 };
-const storage: any = multer.memoryStorage()
+const storage: any = multer.memoryStorage();
 
-const upload = multer({ storage })
+const upload = multer({ storage });
 
 app.get("/", (req: Request, res: Response) => {
-    res.json({ message: "hello" })
-})
+  res.json({ message: "hello" });
+});
 
 app.post("/upload", upload.single("images"), (req: any, res: Response) => {
-    if (req.body.secret === process.env.RAMCHO_SECRET) {
-        if (!req.file) return res.json({ error: "File not found", status: 404 })
-        var filename = createUniqId()
-        const file = new File({ _id: filename, buffer: req.file.buffer, type: req.file.mimetype })
-        file.save((err, file) => {
-            if (err) return res.json({ error: "The file is not saved, something is wrong." });
-            res.json({
-                thumbnail_url: `${process.env.API_URI}/cdn/${filename}.${req.file.mimetype.split("/")[1]}`,
-                url: filename,
-                deletion_url: "none",
-                status: 200
-            });
-        })
-    } else {
-        console.log(req.body.secret);
-        res.json({ error: "Access denied", status: 401 });
-    }
-})
+  if (req.body.secret === process.env.RAMCHO_SECRET) {
+    if (!req.file) return res.json({ error: "File not found", status: 404 });
+    var filename = createUniqId();
+    const file = new File({
+      _id: filename,
+      buffer: req.file.buffer,
+      type: req.file.mimetype,
+    });
+    file.save((err, file) => {
+      if (err)
+        return res.json({
+          error: "The file is not saved, something is wrong.",
+        });
+      res.json({
+        thumbnail_url: `${process.env.API_URI}/cdn/${filename}.${
+          req.file.mimetype.split("/")[1]
+        }`,
+        url: filename,
+        deletion_url: "none",
+        status: 200,
+      });
+    });
+  } else {
+    console.log(req.body.secret);
+    res.json({ error: "Access denied", status: 401 });
+  }
+});
 
 app.get("/i/:id", async (req, res) => {
-    try {
-        const file: any = await File.findById(req.params.id)
-        if (!req.params.id) return res.status(403).json({ error: "Not enough id" })
-        res.json({ filename: file._id, type: file.type, url: `${process.env.API_URI}/cdn/${file._id}.${file.type.split("/")[1]}` })
-    } catch (err) {
-        console.log(err)
-        res.status(403).json({ error: "Not enough id", status: 403 })
-    }
-})
+  try {
+    const file: any = await File.findById(req.params.id);
+    if (!req.params.id) return res.status(403).json({ error: "Not enough id" });
+    res.json({
+      filename: file._id,
+      type: file.type,
+      url: `${process.env.API_URI}/cdn/${file._id}.${file.type.split("/")[1]}`,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(403).json({ error: "Not enough id", status: 403 });
+  }
+});
 app.get("/cdn/:id", async (req: any, res: any) => {
-    try {
-        const fileId = req.params.id.split(".")
-        if (fileId.length === 1) return res.status(404).json({ error: "File not found", status: 404 })
-        if (!fileId[1].match(/^(jpe?g|png|gif|mp4)$/)) return res.status(404).json({ error: "File not found", status: 404 })
-        const file: any = await File.findById(fileId[0])
-        if (file.type.split("/")[1] !== fileId[1]) return res.status(404).json({ error: "File not found", status: 404 })
-        res.end(file.buffer)
-    } catch (err) {
-        console.log(err)
-        res.status(404).json({ error: "File not found", status: 404 })
-    }
-})
+  try {
+    const fileId = req.params.id.split(".");
+    if (fileId.length === 1)
+      return res.status(404).json({ error: "File not found", status: 404 });
+    if (!fileId[1].match(/^(jpe?g|png|gif|mp4)$/))
+      return res.status(404).json({ error: "File not found", status: 404 });
+    const file: any = await File.findById(fileId[0]);
+    if (file.type.split("/")[1] !== fileId[1])
+      return res.status(404).json({ error: "File not found", status: 404 });
+    res.header("Content-Type", "image/" + fileId[1]);
+    res.end(file.buffer);
+  } catch (err) {
+    console.log(err);
+    res.status(404).json({ error: "File not found", status: 404 });
+  }
+});
 
-app.listen(process.env.PORT, () => console.log("ready"))
+app.listen(process.env.PORT, () => console.log("ready"));
